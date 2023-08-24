dd7f4ca8548e1b82f16df615fbf47dde73895ab5
Flare <jntnnn4@gmail.com>
2023-08-23 20:46:20 -0700

    added pause menu

---
+++ Assets/Scripts/Utility/Pause.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Pause : MonoBehaviour
+{
+    public UnityEvent GamePaused;
+    public UnityEvent GameResumed;
+
+    //GameOver gameOver;
+    GameObject mainMenuCanvas;
+
+    public bool IsPaused { get; private set; }
+    public bool IsGameRunning { get; private set; } = false;
+    public bool IsGameOver { get; private set; } = false;
+
+    void Start()
+    {
+        mainMenuCanvas = GameObject.Find("MainMenuCanvas");
+        //gameOver = GetComponent<GameOver>();
+
+        //gameOver.GameOverEvent += HandleGameOver;
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!IsGameOver)
+        {
+            IsGameRunning = true;
+
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                IsPaused = !IsPaused;
+
+                if (IsPaused)
+                {
+                    IsGameRunning = false;
+                    Time.timeScale = 0;
+                    GamePaused.Invoke();
+                }
+                else
+                {
+                    IsGameRunning = true;
+                    Time.timeScale = 1;
+                    GameResumed.Invoke();
+                }
+            }
+        }
+        else
+            IsGameRunning = false;
+    }
+
+    void HandleGameOver()
+    {
+        Debug.Log("Game Over handled");
+        IsGameRunning = false;
+        IsPaused = true;
+        IsGameOver = true;
+    }
+
+    public void PauseGame()
+    {
+        IsPaused = true;
+    }
+
+    public void UnpauseGame()
+    {
+        IsPaused = false;
+    }
+}
